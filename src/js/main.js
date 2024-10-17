6dda6cfdf5eedb29b6a2c80e1d90519e85b2ce22
Daniel Concha <danielconchaparra@gmail.com>
2024-10-17 20:14:52 -0300

    Homepage - Populate post grid with placeholder content

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,45 @@
-import { loadHeaderAndFooter } from "./template-loading/utils.js"
+import { loadHeaderAndFooter, renderDynamicElementList } from "./template-loading/utils.js"
+import PostCard from "./template-loading/PostCard.js"
 
 loadHeaderAndFooter()
+
+
+const posts = [
+    {
+        imgSrc: "/img/placeholder.jpg",
+        title: "This is a title",
+        extract: "If only i knew how to read binary"
+    },
+    {
+        imgSrc: "/img/placeholder.jpg",
+        title: "This is a title",
+        extract: "If only i knew how to read binary"
+    },
+    {
+        imgSrc: "/img/placeholder.jpg",
+        title: "This is a title",
+        extract: "If only i knew how to read binary"
+    },
+    {
+        imgSrc: "/img/placeholder.jpg",
+        title: "This is a title",
+        extract: "If only i knew how to read binary"
+    },
+    {
+        imgSrc: "/img/placeholder.jpg",
+        title: "This is a title",
+        extract: "If only i knew how to read binary"
+    },
+    {
+        imgSrc: "/img/placeholder.jpg",
+        title: "This is a title",
+        extract: "If only i knew how to read binary"
+    }
+]
+
+renderDynamicElementList(
+    {
+        sourceData: posts,
+        parentNode: document.querySelector('[data-dyn-id="post-grid"]'),
+        DynElementClass: PostCard
+    })
