3f07dc04365d04ec7959f2350ddae02756f16de0
Daniel Concha <danielconchaparra@gmail.com>
2024-10-17 19:21:35 -0300

    Add utils for turning html string into node and getting an element on a dynamic template

--- src/js/template-loading/utils.js
+++ src/js/template-loading/utils.js
@@ -1,4 +1,32 @@
 import { TEMPLATE_PATHS } from "../constants.js"
+import { TEMPLATE_DYN_DATA_ATTR_ID } from "../constants.js"
+
+
+// This turns the HTML string into a node,
+// so we can manipulate it more easily
+export function turnTemplateIntoNode(templateString) {
+    const parser = new DOMParser()
+    const tempDoc = parser.parseFromString(templateString, "text/html")
+    return tempDoc.body.childNodes[0]
+}
+
+
+/**
+ * 
+ * @param {HTMLElement} node 
+ * @param {string} dataAttrValue 
+ * @returns {HTMLElement}
+ */
+export function getDynamicElement(node, dataAttrValue) {
+    const cssQuery = `[${TEMPLATE_DYN_DATA_ATTR_ID}=${dataAttrValue}]`
+
+    // If the element itself is the one we want to match
+    // query selector doesn't work, we use matches
+    // to check if the element matches the css query
+    if (node.matches(cssQuery)) return node
+
+    return node.querySelector(cssQuery)
+}
 
 
 export async function loadHeaderAndFooter() {
