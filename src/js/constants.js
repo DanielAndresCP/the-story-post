12b95f08e4f78f93a13b4c3e1db1c9f33286388b
Daniel Concha <danielconchaparra@gmail.com>
2024-10-17 19:20:18 -0300

    Add dynamic template loading constants (path and data attr name)

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,4 +1,7 @@
 export const TEMPLATE_PATHS = {
     header: "/templates/header.html",
-    footer: "/templates/footer.html"
+    footer: "/templates/footer.html",
+    postCard: "/templates/post-card.html"
 }
+
+export const TEMPLATE_DYN_DATA_ATTR_ID = "data-dyn-id"
