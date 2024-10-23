9f9cf24cffb14a3e51cbce8cb1f8b555ca43c3f1
Daniel Concha <danielconchaparra@gmail.com>
2024-10-23 17:24:25 -0300

    Homapage - Add Map with user location
    asd
    
    a

--- src/js/utils.js
+++ src/js/utils.js
@@ -63,6 +63,35 @@ export function getDynamicElement({ node = document.body, elemId }) {
 }
 
 
+function renderMap(data, parentElement) {
+    const { latitude, longitude } = data.coords
+    const map = `<iframe
+    title="Mapa de Chillán"
+    src="https://maps.google.com/maps?q=${latitude},${longitude}&output=embed"
+    class="w-full min-h-72"></iframe>`
+    parentElement.innerHTML = map
+}
+
+
+function showMap(parentElement) {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((e) => {
+            renderMap(e, parentElement)
+        }, () => {
+            parentElement.textContent = "You didn't allow geolocation :("
+        })
+    } else {
+        parentElement.textContent = "Your device doesn't have geolocation capabilities :("
+    }
+}
+
+
+export function setupMap() {
+    const mapContainer = getDynamicElement({ elemId: "map-container" })
+    const mapButton = getDynamicElement({ elemId: "map-button" })
+    mapButton.addEventListener("click", () => { showMap(mapContainer) })
+}
+
 export async function loadHeaderAndFooter() {
     const header = await getTemplate(TEMPLATE_PATHS.header)
     const footerTemplate = await getTemplate(TEMPLATE_PATHS.footer)
