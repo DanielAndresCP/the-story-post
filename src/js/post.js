19d12a55db193abab892c35547b150b972fbddb4
Daniel Concha <danielconchaparra@gmail.com>
2024-10-26 10:14:13 -0300

    Single Post- Integrate with API data

--- src/js/post.js
+++ src/js/post.js
@@ -1,84 +1,59 @@
-import { loadHeaderAndFooter } from "./utils.js"
+import { getSearchParams, loadHeaderAndFooter, turnAPIPostDataIntoSinglePostData, turnAPICommentDataIntoCommentData, turnPostAPIDataIntoPostCardData } from "./utils.js"
 import SinglePost from "./page-content-loading/SinglePost.js"
+import PostData from "./data-fetching/PostData.js"
 
 loadHeaderAndFooter()
 
-// This will be replaced with dynamic content
-const postData = {
-    img: "/img/placeholder.jpg",
-    title: "Dave watched as the forest burned up on the hill.",
-    id: "251",
-    content: `Dave watched as the forest burned up on the hill,
-            only a few miles from her house. The car had been
-            hastily packed and Marta was inside trying to round
-            up the last of the pets. Dave went through his
-            mental list of the most important papers and documents
-            that they couldn't leave behind. He scolded
-            himself for not having prepared these better in
-            advance and hoped that he had remembered everything
-            that was needed. He continued to wait for Marta to
-            appear with the pets, but she still was nowhere
-            to be seen.`,
-    authorCardLink: "/search?author=something",
-    authorCardImg: "/img/placeholder.jpg",
-    authorCardName: "User #16",
-    views: "4152",
-    likes: "1448",
-    dislikes: "39"
+const urlParams = getSearchParams()
+const random = urlParams.get("random")
+const postId = urlParams.get("id")
+
+// If there is no post to get, we redirect to the homepage
+if (!random && !postId) {
+    window.location.pathname = ""
 }
+
+const postAPIDataLogic = new PostData()
+
+// We set post data, depending if a random or specific post was requested
+let postData = null
+if (random) {
+    const apiData = await postAPIDataLogic.getRandomPosts(1)
+
+    postData = turnAPIPostDataIntoSinglePostData(apiData.posts[0])
+} else {
+    const apiData = await postAPIDataLogic.getPost(postId)
+
+    postData = turnAPIPostDataIntoSinglePostData(apiData)
+}
+
+
 const postLogic = new SinglePost(postData)
 postLogic.renderMainContent()
 
-// This will be replaced with dynamic content
-const tags = [
-    { tagName: "magical", tagLink: '/search?tag="magical"' },
-    { tagName: "history", tagLink: '/search?tag="magical"' },
-    { tagName: "french", tagLink: '/search?tag="magical"' }
-]
 
-postLogic.renderTags(tags)
-
-// This will be replaced with dynamic content
-const comments = [
-    {
-        authorUsername: "@jhonDoe",
-        authorDisplayName: "A guy",
-        commentContent: "This is some comment",
-        commentLikes: 784
-    },
-    {
-        authorUsername: "@theMumboJumbo",
-        authorDisplayName: "Mumbo Jumbo",
-        commentContent: "Peace, Love and Plants",
-        commentLikes: 784
-    }
-]
-
+const commentData = await postAPIDataLogic.getPostComments({ postId: postData.id })
+const comments = commentData.comments.map(turnAPICommentDataIntoCommentData)
 postLogic.renderComments(comments)
 
 
-// This will be replaced with dynamic content
-const posts = [
-    {
-        imgSrc: "/img/placeholder.jpg",
-        title: "This is a title",
-        extract: "If only i knew how to read binary"
-    },
-    {
-        imgSrc: "/img/placeholder.jpg",
-        title: "This is a title",
-        extract: "If only i knew how to read binary"
-    },
-    {
-        imgSrc: "/img/placeholder.jpg",
-        title: "This is a title",
-        extract: "If only i knew how to read binary"
-    },
-    {
-        imgSrc: "/img/placeholder.jpg",
-        title: "This is a title",
-        extract: "If only i knew how to read binary"
-    }
-]
+// We get and filter the related posts
+const rawRelatedPosts = []
+for (const tag of postData.tags) {
+    const tagPosts = await postAPIDataLogic.getPostsByTag({ tagId: tag, amount: 3 })
+    rawRelatedPosts.push(...tagPosts.posts)
+}
 
-postLogic.renderRelatedPosts(posts)
+// TODO ADD THIS TO THE SKILL ASSESMENT
+const uniqueRelatedPosts = rawRelatedPosts.filter((x, i, srcArray) => {
+    // We find the index of the first element with this id, and if it is the
+    // the same as the id of the object we are checking, it means that it is the first
+    // appearence, and therefore unique. If it appears again it will not be unique
+    const isFirst = srcArray.findIndex((temp) => temp.id === x.id) === i
+
+    // If it is the same post we are reading, we don't show it
+    const isTheSamePost = postData.id === x.id
+    return isFirst && !isTheSamePost
+})
+
+postLogic.renderRelatedPosts(uniqueRelatedPosts.map(turnPostAPIDataIntoPostCardData))
