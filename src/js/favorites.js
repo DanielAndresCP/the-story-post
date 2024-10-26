69ecf61efe8692b92bf75fbb471ece053eb2d439
Daniel Concha <danielconchaparra@gmail.com>
2024-10-26 16:42:38 -0300

    Favorite Management v01 Heeelp MEEEEE

--- src/js/favorites.js
+++ src/js/favorites.js
@@ -1,105 +1,77 @@
-import { loadHeaderAndFooter, renderDynamicElementList } from "./utils";
+import { generateTagPillData, getDynamicElement, loadHeaderAndFooter, renderDynamicElementList, turnPostAPIDataIntoPostCardData } from "./utils";
 import PostCard from "./template-loading/PostCard";
 import AuthorCard from "./template-loading/AuthorCard"
 import TagPill from "./template-loading/Tag";
+import UserFavorites from "./data-fetching/UserFavorites";
+import PostData from "./data-fetching/PostData";
 
 loadHeaderAndFooter()
 
-const posts = [
-    {
-        imgSrc: "/img/placeholder.jpg",
-        title: "This is a title",
-        extract: "If only i knew how to read binary"
-    },
-    {
-        imgSrc: "/img/placeholder.jpg",
-        title: "This is a title",
-        extract: "If only i knew how to read binary"
-    },
-    {
-        imgSrc: "/img/placeholder.jpg",
-        title: "This is a title",
-        extract: "If only i knew how to read binary"
-    },
-    {
-        imgSrc: "/img/placeholder.jpg",
-        title: "This is a title",
-        extract: "If only i knew how to read binary"
-    },
-    {
-        imgSrc: "/img/placeholder.jpg",
-        title: "This is a title",
-        extract: "If only i knew how to read binary"
-    },
-    {
-        imgSrc: "/img/placeholder.jpg",
-        title: "This is a title",
-        extract: "If only i knew how to read binary"
+const apiData = new PostData()
+
+const favorites = new UserFavorites()
+const { posts: favPosts, users: favAuthors, tags: favTags } = favorites.getUserFavorites()
+
+
+const posts = []
+const authors = []
+const tags = []
+
+
+
+const favPostsContainer = getDynamicElement({ elemId: "favorite-posts-grid" })
+if (favPosts.length > 0) {
+    for (const postId of favPosts) {
+        const postData = await apiData.getPost(postId)
+        posts.push(postData)
     }
-]
 
-renderDynamicElementList(
-    {
-        sourceData: posts,
-        parentNode: document.querySelector('[data-dyn-id="favorite-posts-grid"]'),
-        DynElementClass: PostCard
-    })
+    renderDynamicElementList(
+        {
+            sourceData: posts.map(turnPostAPIDataIntoPostCardData),
+            parentNode: favPostsContainer,
+            DynElementClass: PostCard
+        })
+} else {
+    favPostsContainer.textContent = "No favorite posts"
+}
 
-const authors = [
-    {
-        authorLink: "/search?author=something",
-        authorImgSrc: "/img/placeholder.jpg",
-        authorName: "User #16",
-    },
-    {
-        authorLink: "/search?author=something",
-        authorImgSrc: "/img/placeholder.jpg",
-        authorName: "User #16",
-    },
-    {
-        authorLink: "/search?author=something",
-        authorImgSrc: "/img/placeholder.jpg",
-        authorName: "User #16",
-    },
-    {
-        authorLink: "/search?author=something",
-        authorImgSrc: "/img/placeholder.jpg",
-        authorName: "User #16",
-    },
-    {
-        authorLink: "/search?author=something",
-        authorImgSrc: "/img/placeholder.jpg",
-        authorName: "User #16",
-    },
-    {
-        authorLink: "/search?author=something",
-        authorImgSrc: "/img/placeholder.jpg",
-        authorName: "User #16",
-    },
-    {
-        authorLink: "/search?author=something",
-        authorImgSrc: "/img/placeholder.jpg",
-        authorName: "User #16",
+
+
+const favAuthorsContainer = getDynamicElement({ elemId: "favorite-authors-grid" })
+if (favAuthors.length > 0) {
+    for (const authorId of favAuthors) {
+        const postsByAuthor = await apiData.getPostsByUser({ userId: authorId })
+
+        const { userId } = postsByAuthor.posts[0]
+
+        const authorData = { authorLink: `/search?user=${userId}`, authorImgSrc: "/img/placeholder.jpg", authorName: `User #${userId}` }
+        authors.push(authorData)
     }
-]
 
-renderDynamicElementList(
-    {
-        sourceData: authors,
-        parentNode: document.querySelector('[data-dyn-id="favorite-authors-grid"]'),
-        DynElementClass: AuthorCard
-    })
+    renderDynamicElementList(
+        {
+            sourceData: authors,
+            parentNode: favAuthorsContainer,
+            DynElementClass: AuthorCard
+        })
+} else {
+    favAuthorsContainer.textContent = "No favorite authors"
+}
 
 
-const tags = [
-    { tagName: "magical", tagLink: '/search?tag="magical"' },
-    { tagName: "history", tagLink: '/search?tag="magical"' },
-    { tagName: "french", tagLink: '/search?tag="magical"' }
-]
+const favTagsContainer = getDynamicElement({ elemId: "favorite-tags-list" })
+if (favTags.length > 0) {
+    for (const tag of favTags) {
+        tags.push(generateTagPillData(tag))
+    }
 
-renderDynamicElementList(
-    {
-        sourceData: tags,
-        parentNode: document.querySelector('[data-dyn-id="favorite-tags-list"]'),
-        DynElementClass: TagPill
-    })
+    renderDynamicElementList(
+        {
+            sourceData: tags,
+            parentNode: favTagsContainer,
+            DynElementClass: TagPill
+        })
+} else {
+    favTagsContainer.textContent = "No favorite tags"
+}
