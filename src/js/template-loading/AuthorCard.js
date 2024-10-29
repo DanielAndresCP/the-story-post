8c23069eefe4904251756c2dc375d46192200d22
Daniel Concha <danielconchaparra@gmail.com>
2024-10-28 21:07:31 -0300

    Author Card - Integrate with Image Fetching module

--- src/js/template-loading/AuthorCard.js
+++ src/js/template-loading/AuthorCard.js
@@ -1,4 +1,5 @@
-import { TEMPLATE_PATHS } from "../constants.js";
+import { PLACEHOLDER_IMG_PATH, TEMPLATE_PATHS } from "../constants.js";
+import ImageData from "../data-fetching/ImageData.js";
 import { getTemplate, turnTemplateIntoNode, getDynamicElement } from "../utils.js";
 
 export default class AuthorCard {
@@ -14,14 +15,25 @@ export default class AuthorCard {
 
     constructor({ authorLink, authorImgSrc, authorName }) {
         this.authorLink = authorLink
-        this.authorImgSrc = authorImgSrc
+        this.authorImgSrc = authorImgSrc !== PLACEHOLDER_IMG_PATH ? authorImgSrc : undefined
         this.authorName = authorName
     }
 
 
     fillDynamicContent() {
         getDynamicElement({ node: this.authorCardEl, elemId: this.dynContentIds.link }).attributes.href.value = this.authorLink
-        getDynamicElement({ node: this.authorCardEl, elemId: this.dynContentIds.imgSrc }).attributes.src.value = this.authorImgSrc
+
+        const imgEl = getDynamicElement({ node: this.authorCardEl, elemId: this.dynContentIds.imgSrc })
+
+        if (this.authorImgSrc) {
+            imgEl.attributes.src.value = this.authorImgSrc
+        } else {
+            const imageLogic = new ImageData()
+            const seed = imageLogic.getSeedFromString(this.authorName)
+            imageLogic.getImageURL({ seed, width: 56 }).then((x) => { imgEl.attributes.src.value = x })
+        }
+
+
         getDynamicElement({ node: this.authorCardEl, elemId: this.dynContentIds.imgSrc }).attributes.alt.value = `Profile Photo for ${this.authorName}`
         getDynamicElement({ node: this.authorCardEl, elemId: this.dynContentIds.name }).textContent = this.authorName
     }
